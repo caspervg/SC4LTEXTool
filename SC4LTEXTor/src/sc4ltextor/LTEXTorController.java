b9201f496336f7c1344c888ac70e600c573e7992
Casper <Casper@Casper-Netbook>
2013-02-28 11:05:54 +0100

    Added batch GID increments with jdenm8's overflow checker, added lastaction label

--- SC4LTEXTor/src/sc4ltextor/LTEXTorController.java
+++ SC4LTEXTor/src/sc4ltextor/LTEXTorController.java
@@ -14,18 +14,27 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBoxBuilder;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import ssp.dbpf.DBPFCollection;
 import ssp.dbpf.event.DBPFException;
 import ssp.dbpf.io.DBPFReader;
 import ssp.dbpf.io.DBPFWriter;
+import ssp.dbpf.tgi.TGIKey;
 import ssp.dbpf.tgi.TGIKeys;
 import ssp.dbpf.types.DBPFLText;
 import ssp.dbpf.types.DBPFType;
@@ -46,7 +55,35 @@ public class LTEXTorController implements Initializable {
     public TableView<LTTable> table;
     private final ObservableList<LTTable> tabledata = FXCollections.observableArrayList();
     public TableColumn gidcol, iidcol, contcol, indexcol;
-    public TextField transin, transout;
+    public TextField transin, transout, incr;
+    public Label lastaction;
+
+    public void handleIncrement() throws DBPFException {
+        long maxLength = Long.parseLong("4294967280");
+        long increment = Long.parseLong(incr.getText());
+        for (DBPFType type : typeList) {
+            long currGID = type.getGID();
+            if (currGID + increment >= maxLength) {
+                Stage dialogStage = new Stage();
+                dialogStage.initModality(Modality.WINDOW_MODAL);
+                dialogStage.setScene(new Scene(VBoxBuilder.create().
+                        children(new Text("Could not increment the Group IDs. One (or more) entries have a GID higher than the maximum value of 0xFFFFFFF0"), new Button("OK")).
+                        alignment(Pos.CENTER).padding(new Insets(5)).build()));
+                dialogStage.show();
+                lastaction.setText("Incrementing failed");
+                return;
+            }
+        }
+        for (DBPFType type : typeList) {
+            long currGID = type.getGID();
+            if (currGID + increment < maxLength) {
+                type.setTGIKey(new TGIKey(type.getTID(), type.getGID() + increment, type.getIID()));
+            }
+        }
+        lastaction.setText("Incremented GID by " + increment);
+        
+        readDataIntoTable(true);
+    }
 
     public void handleOpenButton() throws DBPFException {
         FileChooser fc = new FileChooser();
@@ -63,6 +100,7 @@ public class LTEXTorController implements Initializable {
         file = fc.showOpenDialog(null);
         theDAT = DBPFReader.readCollection(file);
 
+        lastaction.setText("Opened " + theDAT.getFilename().getName());
         readDataIntoTable(false);
     }
 
@@ -83,22 +121,27 @@ public class LTEXTorController implements Initializable {
         if (file != null) {
             theDAT.setFilename(file);
             DBPFWriter.writeCollection(theDAT);
+            lastaction.setText("Saved " + file.getName());
         }
     }
-    
+
     public void handleBatchTranslation() {
         typeList = theDAT.getTypeList();
         String transintext = transin.getText();
         String transouttext = transout.getText();
+        int counter = 0;
+        
         for (int i = 0; i < typeList.size(); i++) {
             DBPFType type = typeList.get(i);
 
             if (type.getTGIKey().equals(TGIKeys.LTEXT.getTGIKey())) {
                 DBPFLText ltext = (DBPFLText) type;
                 ltext.setString(ltext.getString().replace(transintext, transouttext));
+                counter++;
             }
         }
-        
+
+        lastaction.setText("Translated " + counter + " LTEXTs");
         //Refresh the table
         readDataIntoTable(true);
     }
@@ -109,8 +152,10 @@ public class LTEXTorController implements Initializable {
     }
 
     private void readDataIntoTable(boolean refreshFirst) {
-        if(refreshFirst) tabledata.clear();
-        
+        if (refreshFirst) {
+            tabledata.clear();
+        }
+
         typeList = theDAT.getTypeList();
         for (int i = 0; i < typeList.size(); i++) {
             DBPFType type = typeList.get(i);
@@ -122,11 +167,11 @@ public class LTEXTorController implements Initializable {
         table.setEditable(true);
         Callback<TableColumn, TableCell> cellFactory =
                 new Callback<TableColumn, TableCell>() {
-            @Override
-            public TableCell call(TableColumn p) {
-                return new EditingCell();
-            }
-        };
+                    @Override
+                    public TableCell call(TableColumn p) {
+                        return new EditingCell();
+                    }
+                };
 
         gidcol.setCellValueFactory(new PropertyValueFactory<LTTable, String>("gid"));
         iidcol.setCellValueFactory(new PropertyValueFactory<LTTable, String>("iid"));
@@ -135,15 +180,16 @@ public class LTEXTorController implements Initializable {
         //--- Add for Editable Cell of Value field, in Double
         contcol.setCellFactory(cellFactory);
         contcol.setOnEditCommit(
-            new EventHandler<TableColumn.CellEditEvent<LTTable, String>>() {
-            @Override
-            public void handle(TableColumn.CellEditEvent<LTTable, String> t) {
-                String newValue = t.getNewValue();
-                ((LTTable) t.getTableView().getItems().get(t.getTablePosition().getRow())).setString(newValue);
-                //Sets the actual DBPF string to the new string
-                ((DBPFLText) theDAT.getTypeList().get(t.getTableView().getItems().get(t.getTablePosition().getRow()).getIndex())).setString(newValue);
-            }
-        });
+                new EventHandler<TableColumn.CellEditEvent<LTTable, String>>() {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<LTTable, String> t) {
+                        String newValue = t.getNewValue();
+                        ((LTTable) t.getTableView().getItems().get(t.getTablePosition().getRow())).setString(newValue);
+                        //Sets the actual DBPF string to the new string
+                        ((DBPFLText) theDAT.getTypeList().get(t.getTableView().getItems().get(t.getTablePosition().getRow()).getIndex())).setString(newValue);
+                        lastaction.setText("Edited row " + t.getTableView().getItems().get(t.getTablePosition().getRow()).getIndex());
+                    }
+                });
         //---
         contcol.setEditable(true);
         contcol.setResizable(true);
